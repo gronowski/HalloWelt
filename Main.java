997421a5e6cbc9a299b7a009ddd0fa51c9130195
gronowski <terence.gronowski@gmail.com>
2013-11-08 14:00:09 +0100

    Erster Commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Terence
+ * Date: 08.11.13
+ * Time: 09:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hallo Welt");
+        //Bildschirmausgabe
+
+    }
+}
